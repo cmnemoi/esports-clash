eec564464513a97eda6567422a300e7d7fd3c222
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-04-07 19:31:27 +0200

    wip: E2E tests

--- src/test/java/fr/cmnemoi/esportsclash/PostgreSqlTestConfiguration.java
+++ src/test/java/fr/cmnemoi/esportsclash/PostgreSqlTestConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -12,5 +13,10 @@ public class PostgreSqlTestConfiguration {
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
 		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	@Bean
+	ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 }
